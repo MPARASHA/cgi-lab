2ba37850c02ad3e6933d8aeef0e410c3c360c0ff
Neil <mparasha@ualberta.ca>
2021-09-23 17:15:17 -0600

    Q1 and Q2

---
+++ hello.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import os, json
+import cgitb
+cgitb.enable()
+
+print("Content-type:text/html\r\n\r\n")
+print()
+print("<title>Test CGI</title>")
+print("<p>Hello World!</p>")
+
+# Q1
+json_object = json.dumps(dict(os.environ), indent=4)
+print(json_object)
+
+#Q2
+for param in os.environ.keys():
+    if param == "QUERY_STRING":
+        print("<br><br><b>%20s</b>: %s<b>" % (param, os.environ[param]))
